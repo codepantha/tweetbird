b7c74c4a7859218da8d523ab9eaf6788d908d42c
codepanther <pezechigozirim@gmail.com>
2022-10-18 16:38:53 +0100

    Create Feed component skeleton

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,7 @@
 import type { NextPage } from 'next'
 import Head from 'next/head'
 import Image from 'next/image'
+import Feed from '../components/Feed'
 import Sidebar from '../components/Sidebar'
 
 const Home: NextPage = () => {
@@ -11,7 +12,7 @@ const Home: NextPage = () => {
       </Head>
       <main>
         <Sidebar />
-        {/* Feed */}
+        <Feed />
         {/* Widgets */}
       </main>
     </div>

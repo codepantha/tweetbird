a15a37fad898b5f30d0bbc41458e318222087b48
codepanther <pezechigozirim@gmail.com>
2022-10-23 23:42:57 +0100

    Implement refresh tweets functionality and display toast notifications

--- components/Feed.tsx
+++ components/Feed.tsx
@@ -1,25 +1,44 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { FiRefreshCcw } from 'react-icons/fi';
+import toast from 'react-hot-toast';
+
 import { Tweet } from '../typings';
 import TweetBox from './TweetBox';
 import TweetComponent from './Tweet';
+import { fetchTweets } from '../utils/fetchTweets';
 
 interface Props {
   tweets: Tweet[];
 }
 
-function Feed({ tweets }: Props) {
+function Feed({ tweets: tweetsProp }: Props) {
+  const [tweets, setTweets] = useState<Tweet[]>(tweetsProp);
+
+  const handleRefresh = async () => {
+    const refreshToast = toast.loading('Refreshing...');
+
+    const tweets: Tweet[] = await fetchTweets();
+    setTweets(tweets);
+
+    toast.success('Feed Updated', {
+      id: refreshToast
+    });
+  };
+
   return (
     <div className="col-span-7 lg:col-span-5 border-x">
       <div className="flex justify-between">
         <h1 className="p-5 pb-0 text-xl font-bold">Home</h1>
-        <FiRefreshCcw className="mr-5 mt-5 text-xl h-8 w-8 cursor-pointer text-twitter transition-all duration-500 ease-out hover:rotate-180 active:scale-125" />
+        <FiRefreshCcw
+          onClick={handleRefresh}
+          className="mr-5 mt-5 text-xl h-8 w-8 cursor-pointer text-twitter transition-all duration-500 ease-out hover:rotate-180 active:scale-125"
+        />
       </div>
 
       <TweetBox />
 
-      {tweets.map(tweet => (
-        <TweetComponent tweet={tweet} />
+      {tweets.map((tweet) => (
+        <TweetComponent key={tweet._id} tweet={tweet} />
       ))}
     </div>
   );
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,41 +1,42 @@
-import type { GetServerSideProps, NextPage } from 'next'
-import Head from 'next/head'
-import Image from 'next/image'
+import type { GetServerSideProps, NextPage } from 'next';
+import Head from 'next/head';
+import Image from 'next/image';
 
-import Feed from '../components/Feed'
-import Sidebar from '../components/Sidebar'
-import Widgets from '../components/Widgets'
-import { Tweet } from '../typings'
-import { fetchTweets } from '../utils/fetchTweets'
+import Feed from '../components/Feed';
+import Sidebar from '../components/Sidebar';
+import Widgets from '../components/Widgets';
+import { Tweet } from '../typings';
+import { fetchTweets } from '../utils/fetchTweets';
+import { Toaster } from 'react-hot-toast';
 
 interface Props {
   tweets: Tweet[];
 }
 
 const Home = ({ tweets }: Props) => {
-  console.log(tweets);
   return (
     <div className="mx-auto max-h-screen lg:max-w-6xl">
       <Head>
         <title>TweetBird</title>
       </Head>
+      <Toaster />
       <main className="grid grid-cols-9">
         <Sidebar />
         <Feed tweets={tweets} />
         <Widgets />
       </main>
     </div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
 
 export const getServerSideProps: GetServerSideProps = async (context) => {
-  const tweets = await fetchTweets()
+  const tweets = await fetchTweets();
 
   return {
     props: {
       tweets
     }
-  }
-}
+  };
+};

1732f69d2123fee926a2a7a5a11dc9f30aa3b242
codepanther <pezechigozirim@gmail.com>
2022-10-24 14:51:07 +0100

    Add signIn and signOut functionality with next-auth

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import { useSession, signIn, signOut } from 'next-auth/react';
 import {
   BellIcon,
   BookmarkIcon,
@@ -12,6 +13,8 @@ import {
 import SidebarRow from './SidebarRow';
 
 function Sidebar(): JSX.Element {
+  const { data: session } = useSession();
+
   return (
     <div className="col-span-2 items-center md:items-start flex flex-col">
       <img src="/images/logo.svg" alt="logo" className="h-8 w-10" />
@@ -20,7 +23,7 @@ function Sidebar(): JSX.Element {
       <SidebarRow Icon={BellIcon} title="Notifications" />
       <SidebarRow Icon={ChatBubbleLeftIcon} title="Messages" />
       <SidebarRow Icon={BookmarkIcon} title="Bookmarks" />
-      <SidebarRow Icon={UserIcon} title="Sign In" />
+      <SidebarRow onClick={session ? signOut : signIn} Icon={UserIcon} title={session ? 'Sign Out' : 'Sign In'} />
       <SidebarRow Icon={EllipsisHorizontalIcon} title="More" />
     </div>
   )
--- components/SidebarRow.tsx
+++ components/SidebarRow.tsx
@@ -3,11 +3,12 @@ import React, { SVGProps } from 'react';
 interface Props {
   Icon: (props: SVGProps<SVGSVGElement>) => JSX.Element;
   title: string;
+  onClick?: () => {}
 }
 
-function SidebarRow({ Icon, title }: Props) {
+function SidebarRow({ Icon, title, onClick }: Props) {
   return (
-    <div className="flex items-center max-w-fit space-x-2 rounded-full px-4 py-3 transition-all duration-200 cursor-pointer hover:bg-gray-100 group">
+    <div onClick={() => onClick?.()} className="flex items-center max-w-fit space-x-2 rounded-full px-4 py-3 transition-all duration-200 cursor-pointer hover:bg-gray-100 group">
       <Icon className="h-6 w-6" />
       <p className="hidden md:inline-flex text-base font-light lg:text-xl group-hover:text-twitter">{title}</p>
     </div>

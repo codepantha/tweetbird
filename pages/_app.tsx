5891217aaa4f5ec9467941a9f3ba8772d3c36a0d
codepanther <pezechigozirim@gmail.com>
2022-10-25 11:56:34 +0100

    Fix Property 'session' does not exist on type '{}' error

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,12 @@
 import { SessionProvider } from 'next-auth/react';
 import '../styles/globals.css';
 import type { AppProps } from 'next/app';
+import { Session } from 'next-auth';
 
-function MyApp({ Component, pageProps: { session, ...pageProps } }: AppProps) {
+function MyApp({
+  Component,
+  pageProps: { session, ...pageProps }
+}: AppProps<{ session: Session }>) {
   return (
     <SessionProvider session={session}>
       <Component {...pageProps} />
